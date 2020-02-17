37cbc33ff3301783f3c19da5fc3696304b2d43fa
linguang cheng <chenglinguang123@163.com>
2020-02-17 15:02:02 +0000

    adding the back with syncmutex

---
+++ bank_syncmutex.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	mu sync.Mutex //guards balance
+	balance int
+)
+
+func Deposit(amount int){
+	mu.Lock()
+	balance = balance + amount
+	mu.Unlock
+}
+
+func Balance() int{
+	mu.Lock()
+	b := balance
+	mu.Unlock()
+	return b
+}
+
+
