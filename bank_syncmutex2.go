48a0ce93497794418eeb12e0fda85602ba384ad6
linguang cheng <chenglinguang123@163.com>
2020-02-18 00:59:06 +0000

    use a small function for deposit amount

---
+++ bank_syncmutex2.go
@@ -0,0 +1,27 @@
+func Withdraw(amount int) bool{
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(-amount)
+	if balance < 0 {
+		deposit(amount)
+		return false
+	}
+	return true
+}
+
+func Deposit(amount int){
+	mu.Lock()
+	defer mu.Unlock()
+	deposit(amount)
+}
+
+func Balance() int{
+	mu.Lock()
+	defer mu.Unlock()
+	return balance
+}
+
+// This function requires that the lock be held.
+func deposit(amount int){
+	balance += amount
+}
